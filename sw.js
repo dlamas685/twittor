5fa9c71abc99a99c210c62526a2fc66d92a842b2
Daniel Lamas <dlamas1298@gmail.com>
2023-10-19 16:45:56 -0300

    Modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -14,8 +14,8 @@ importScripts('js/sw-utils.js');
     * forma dinamica (dinamicos)
  */
 
-const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v3';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 // !Se puede usar con "/" o sin al inicio
@@ -59,6 +59,10 @@ self.addEventListener('activate', event => {
             if (key !== STATIC_CACHE && key.includes('static')) {
                return caches.delete(key);
             }
+
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
+                return caches.delete(key);
+             }
         })
     });
 
