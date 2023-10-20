b7442d963bf160bbecd0fd90f65486ddd2eafea6
Daniel Lamas <dlamas1298@gmail.com>
2023-10-20 15:14:48 -0300

    Revertidas las versiones de cache

--- sw.js
+++ sw.js
@@ -14,8 +14,8 @@ importScripts('js/sw-utils.js');
     * forma dinamica (dinamicos)
  */
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v2';
+const STATIC_CACHE = 'static-v1';
+const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 // !Se puede usar con "/" o sin al inicio
